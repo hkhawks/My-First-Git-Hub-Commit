f9a51fd4778e75067364af4c5a41b3bbd73ad5c1
haleem <h.kareemy@gmail.com>
2021-10-28 23:03:23 -0700

    commit

---
+++ GitProjectPractice/src/TestClass.java
@@ -0,0 +1,11 @@
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is test for git");
+		System.out.println("this is first commit in Git Hub");
+
+	}
+
+}
